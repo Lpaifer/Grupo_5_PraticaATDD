778d1e9e98f2a069e1f35936e28d6301b3e8ab79
Matheus <matheus.ca12@gmail.com>
2026-09-09 20:35:11 -0300

    criacao de branch

--- src/test/java/org/example/grupo_5_praticaatdd/Curso.java
+++ src/test/java/org/example/grupo_5_praticaatdd/Curso.java
@@ -2,8 +2,25 @@ package org.example.grupo_5_praticaatdd;
 
 import org.junit.jupiter.api.Test;
 
-public class Curso {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CursoTest {
 
     @Test
+    public void deveCriarCursoComNomeECargaHoraria() {
 
+        // ARRANGE
+        String nomeEsperado = "Engenharia da Computação";
+        int cargaHorariaEsperada = 3600;
+
+        Curso curso = new Curso(nomeEsperado, cargaHorariaEsperada);
+
+        // ACTION
+        String nomeObtido = curso.getNome();
+        int cargaHorariaObtida = curso.getCargaHoraria();
+
+        // ASSERT
+        assertEquals(nomeEsperado, nomeObtido);
+        assertEquals(cargaHorariaEsperada, cargaHorariaObtida);
+    }
 }
