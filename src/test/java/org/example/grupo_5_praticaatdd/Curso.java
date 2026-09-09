f4d11052e0bbe2fef1f19712dba891b48aa4693e
canaveze <beatrizfontolansoares@gmail.com>
2026-09-09 20:15:32 -0300

    Criação de branch

--- src/test/java/org/example/grupo_5_praticaatdd/Curso.java
+++ src/test/java/org/example/grupo_5_praticaatdd/Curso.java
@@ -1,5 +1,11 @@
 package org.example.grupo_5_praticaatdd;
 
+import org.junit.jupiter.api.Test;
+
 public class Curso {
+
     @Test
+    public void meuPrimeiroTeste() {
+        // Escreva a lógica do seu teste aqui
+    }
 }
