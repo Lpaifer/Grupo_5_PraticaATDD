249f4f159b890ea930c885d044d3f4ceddf40d8a
Leonardo Godinho da Silva <godinho1600@gmail.com>
2026-09-12 11:56:39 -0300

    GREEN - implementar acumulo de cursos adicionais

--- src/test/java/org/example/grupo_5_praticaatdd/domain/PlataformaGamificacaoTest.java
+++ src/test/java/org/example/grupo_5_praticaatdd/domain/PlataformaGamificacaoTest.java
@@ -6,17 +6,39 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlataformaGamificacaoTest {
 
-@Test
-public void deveLiberarTresCursosQuandoMediaForExatamente7() {
-    var plataforma = new PlataformaGamificacao();
-    var aluno = new Aluno("João", Plano.Basico);
-    var curso = new Curso("Curso 1");
-    var conclusao = new ConclusaoCurso(aluno, curso, 7.0, true);
+    @Test
+    public void deveLiberarTresCursosQuandoMediaForExatamente7() {
+        var plataforma = new PlataformaGamificacao();
+        var aluno = new Aluno("João", Plano.Basico);
+        var curso = new Curso("Curso 1");
+        var conclusao = new ConclusaoCurso(aluno, curso, 7.0, true);
 
-    plataforma.processarConclusao(conclusao);
+        plataforma.processarConclusao(conclusao);
 
-    assertEquals(3, aluno.getCursoAdicionaisLiberados());
-}
+        assertEquals(3, aluno.getCursoAdicionaisLiberados());
+    }
 
+    @Test
+    void deveAcumularSeisCursosAposSegundaAprovacao() {
 
+        // ARRANGE
+        var plataforma = new PlataformaGamificacao();
+        var aluno = new Aluno("Leonardo", Plano.Basico);
+
+        var primeiroCurso = new Curso("Curso 1");
+        var primeiraConclusao =
+                new ConclusaoCurso(aluno, primeiroCurso, 8.0, true);
+
+        plataforma.processarConclusao(primeiraConclusao);
+
+        var segundoCurso = new Curso("Curso 2");
+        var segundaConclusao =
+                new ConclusaoCurso(aluno, segundoCurso, 7.5, true);
+
+        // ACT
+        plataforma.processarConclusao(segundaConclusao);
+
+        // ASSERT
+        assertEquals(6, aluno.getCursoAdicionaisLiberados());
+    }
 }
